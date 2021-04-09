5cc8a5efb6e5ede479b3abbe5819c5c7bedbb82f
RuJIA <rujiacs@gmail.com>
2021-04-09 18:47:20 +0800

    add parser script
    Signed-off-by: RuJIA <rujiacs@gmail.com>

---
+++ parse_raw_record.py
@@ -0,0 +1,26 @@
+import struct
+import sys
+import os
+
+def get_plain_filename(rawfile):
+    (name, ext) = os.path.splitext(rawfile)
+    return name + "_lat.txt"
+
+def parse_raw(rawfile):
+    outfile = open(get_plain_filename(rawfile), mode='w')
+    infile = open(rawfile, mode="rb")
+    byte = infile.read(24)
+    while byte:
+        (id, tx, rx) = struct.unpack("<3Q", byte)
+        outfile.write("{0}\t{1}\n".format(id, int((rx - tx) / 2.1)))
+        byte = infile.read(24)
+    infile.close()
+    outfile.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Usage: python ./parse_raw_record.py <raw record file>")
+    else:
+        rawfile = str(sys.argv[1])
+        parse_raw(rawfile)
